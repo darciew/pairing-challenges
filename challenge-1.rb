1d7baae30b309daf4a8cfc85690d5e8c96e3c906
Darcie <darciewalsh@hotmail.com>
2018-09-03 17:02:31 +0100

    added solution and challenge2

--- challenge-1.rb
+++ challenge-1.rb
@@ -1,6 +1,6 @@
 #1
 arr = [1, 2, 3, 4, 5]
-arr1 = arr.push(6)
+arr1 = arr.push(6).drop(1)
 print arr1
 
 
@@ -8,10 +8,13 @@ print arr1
 print [1, 3, 5, 4, 2].sort
 
 #3
-[1, 3, 5, 4, 2].sort.map { |num| print num + 1 }
+new_arr = [1, 3, 5, 4, 2].push(6).sort.drop(1)
+print new_arr
 
 #4
 print [1, 2, 3, 4, 5].sum
 
 #5
-[1, 2, 3, 4, 5].map { |num| print num * 2}
+another_arr = [1, 2, 3, 4, 5].sum
+sum = another_arr * 2
+print sum
